01aa52c4526902cbb5ac1da605b08fb2421363e6
jiaody111 <535413629@qq.com>
2020-03-02 02:01:24 +0800

    更新API

---
+++ miniprogram/pages/GBDT/GBDT.js
@@ -0,0 +1,69 @@
+// pages/GBDT/GBDT.js
+var util = require('../../util/util.js');
+var app = getApp();
+Page({
+  data: {
+
+  },
+  onLoad: function (options) {
+
+  },
+  formsubmit(e) {
+    util.reqFunc("https://api.phmlearn.com/component/ML/classify/7",
+      {
+        "access_token": app.globalData.access_token,
+        "file_name": app.globalData.output_fileName,
+      },'result',function(res){
+        app.globalData.resultArray = [res.data.data.accuracy, res.data.data.recall, res.data.data.precision, res.data.data.fMeasure, res.data.data.rocArea];
+        console.log(app.globalData.resultArray);
+      })
+  },
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
